2bf1d12dd970c6a13b01c5cbad59fd8f9e4f602c
curyPD <romandruzhinin2003@gmail.com>
2022-09-25 13:43:46 +0300

    Update logo, create mobile nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,111 @@
-import React from "react";
-import logo from "../img/logo.png";
+import React, { useState } from "react";
+import { HiOutlineMenu, HiOutlineX } from "react-icons/hi";
 
 function Header({ handleScrollRewards, handleScrollWork }) {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    function openMenu() {
+        setMenuOpen(true);
+    }
+
+    function closeMenu() {
+        setMenuOpen(false);
+    }
+
     return (
-        <header>
-            <div className="mx-auto flex h-20 max-w-screen-2xl items-center justify-between px-10">
-                <div>
-                    <img
-                        src={logo}
-                        alt="Orange letters C and D"
-                        className="h-16"
-                    />
+        <header className="relative">
+            <div
+                onClick={closeMenu}
+                className={`absolute right-0 top-0 z-40 block transition-all ${
+                    menuOpen
+                        ? "pointer-events-auto visible opacity-100"
+                        : "pointer-events-none invisible opacity-0"
+                } h-screen w-screen touch-none bg-gray-900/60 backdrop-blur-sm lg:hidden`}
+            ></div>
+
+            <div
+                className={`absolute right-0 top-0 z-50 block h-screen w-64 transition-all ${
+                    menuOpen
+                        ? "pointer-events-auto translate-x-0"
+                        : "pointer-events-none translate-x-full"
+                } touch-none bg-white py-3 pr-4 pl-6 md:w-80 md:pr-8 lg:hidden`}
+            >
+                <div className="mb-10 flex justify-end">
+                    <button
+                        onClick={closeMenu}
+                        className="flex h-10 w-10 items-center justify-center rounded-full focus:outline-none focus:ring-4 focus:ring-orange-200"
+                    >
+                        <HiOutlineX className="text-3xl text-orange-400" />
+                    </button>
                 </div>
                 <nav>
-                    <ol className="flex items-center gap-10">
-                        <li className="group text-lg font-normal">
+                    <ol className="flex flex-col gap-12">
+                        <li className="group text-2xl font-medium">
                             <button
-                                onClick={handleScrollRewards}
-                                className="text-gray-600 transition-colors group-hover:text-orange-400"
+                                onClick={() => {
+                                    closeMenu();
+                                    handleScrollRewards();
+                                }}
+                                className="text-gray-700 transition-colors focus:outline-none focus:ring-2 focus:ring-orange-200 group-hover:text-orange-400"
                             >
                                 Дипломы
                             </button>
                         </li>
-                        <li className="group text-lg font-normal">
+                        <li className="group text-2xl font-medium">
                             <button
-                                onClick={handleScrollWork}
-                                className="text-gray-600 transition-colors group-hover:text-orange-400"
+                                onClick={() => {
+                                    closeMenu();
+                                    handleScrollWork();
+                                }}
+                                className="text-gray-700 transition-colors focus:outline-none focus:ring-2 focus:ring-orange-200 group-hover:text-orange-400"
                             >
                                 Произведения
                             </button>
                         </li>
-                        <li className="group text-lg font-normal">
+                        <li className="group text-2xl font-medium">
                             <a
-                                className="text-gray-600 transition-colors group-hover:text-orange-400"
+                                className="text-gray-700 transition-colors focus:outline-none focus:ring-2 focus:ring-orange-200 group-hover:text-orange-400"
+                                href="mailto: stas1365iv@gmail.com"
+                            >
+                                Написать
+                            </a>
+                        </li>
+                    </ol>
+                </nav>
+            </div>
+
+            <div className="mx-auto flex h-16 max-w-screen-xl items-center justify-end px-4 md:px-8 lg:px-12">
+                {!menuOpen && (
+                    <div className="block lg:hidden">
+                        <button
+                            onClick={openMenu}
+                            className="flex h-10 w-10 items-center justify-center rounded-full focus:outline-none focus:ring-4 focus:ring-orange-200"
+                        >
+                            <HiOutlineMenu className="text-3xl text-orange-400" />
+                        </button>
+                    </div>
+                )}
+                <nav className="hidden lg:block">
+                    <ol className="flex items-center gap-11">
+                        <li className="group text-lg font-normal lg:text-lg xl:text-xl">
+                            <button
+                                onClick={handleScrollRewards}
+                                className="text-gray-600 transition-colors focus:outline-none focus:ring-2 focus:ring-orange-200 group-hover:text-orange-400"
+                            >
+                                Дипломы
+                            </button>
+                        </li>
+                        <li className="group text-lg font-normal lg:text-lg xl:text-xl">
+                            <button
+                                onClick={handleScrollWork}
+                                className="text-gray-600 transition-colors focus:outline-none focus:ring-2 focus:ring-orange-200 group-hover:text-orange-400"
+                            >
+                                Произведения
+                            </button>
+                        </li>
+                        <li className="group text-lg font-normal lg:text-lg xl:text-xl">
+                            <a
+                                className="text-gray-600 transition-colors focus:outline-none focus:ring-2 focus:ring-orange-200 group-hover:text-orange-400"
                                 href="mailto: stas1365iv@gmail.com"
                             >
                                 Написать
